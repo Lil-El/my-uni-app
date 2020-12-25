2dec112f485e63f3577fd6de35bc38ca81ad171a
Lil-El <yxd99324@qq.com>
2020-12-25 22:58:12 +0800

    list

--- components/uni-card/uni-card.vue
+++ components/uni-card/uni-card.vue
@@ -1,13 +1,20 @@
 <template>
-	<view class="uni-card uni-padding-wrap"><slot></slot></view>
+	<view class="uni-card uni-padding-wrap" @click="handleClick">
+		<slot></slot>
+	</view>
 </template>
 <script>
-export default {
-	name: 'uni-card'
-};
+	export default {
+		name: 'uni-card',
+		methods: {
+			handleClick() {
+				this.$listeners.click && this.$listeners.click();
+			}
+		}
+	};
 </script>
 <style scoped lang="scss">
-.uni-card {
-	margin-bottom: 20rpx;
-}
+	.uni-card {
+		margin-bottom: 20rpx;
+	}
 </style>
